83955001a37bad435415f68ba277b8e73cff2c18
Michael Mainer <MIchaelMainer@users.noreply.github.com>
2020-07-30 11:57:40 -0700

    [skip ci] Update to 1.14.1 for bug fix in NullableOption export (#160)
    * Update to 1.14.1 for bug fix in NullableOption export
    Skipping CI so we don't kick off auto-build workflow.

--- microsoft-graph.d.ts
+++ microsoft-graph.d.ts
@@ -1,16 +1,17 @@
-// Type definitions for non-npm package microsoft-graph 1.14
+// Type definitions for non-npm package microsoft-graph 1.14.1
 // Project: https://github.com/microsoftgraph/msgraph-typescript-typings
 // Definitions by: Microsoft Graph Team <https://github.com/microsoftgraph>
 //                 Michael Mainer <https://github.com/MIchaelMainer>
 //                 Peter Ombwa <https://github.com/peombwa>
 //                 Mustafa Zengin <https://github.com/zengin>
 //                 DeVere Dyett <https://github.com/ddyett>
+//                 Nikitha Udaykumar Chettiar <https://github.com/nikithauc>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.1
 
 export as namespace microsoftgraph;
 
-type NullableOption<T> = T | null;
+export type NullableOption<T> = T | null;
 
 export type RiskLevel = "low" | "medium" | "high" | "hidden" | "none" | "unknownFutureValue";
 export type AppliedConditionalAccessPolicyResult =
